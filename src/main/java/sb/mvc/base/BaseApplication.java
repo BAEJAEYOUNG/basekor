e0b6eee4e1a0e7fd11fbcf40a8be40d65801c49f
BAEJAEYOUNG <bjyljs@gmail.com>
2019-06-03 11:04:28 +0900

    test commit

---
+++ src/main/java/sb/mvc/base/BaseApplication.java
@@ -0,0 +1,17 @@
+package sb.mvc.base;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@Slf4j
+@SpringBootApplication
+public class BaseApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(BaseApplication.class, args);
+
+    }
+
+}
